059a33666da47fa8ee65e06103906e4c62a6810a
Till <till.bruegmann@icloud.com>
2025-06-06 19:46:23 +0200

    init

---
+++ jwttoken.py
@@ -0,0 +1,34 @@
+import jwt
+import time
+
+def get_token():
+    KEY_ID = "9JWW26X9GC"
+    PRIVATE_KEY_PATH = "/Users/till/Desktop/AppStore Connect API Test/AuthKey_9JWW26X9GC.p8"
+
+    with open(PRIVATE_KEY_PATH, 'r') as f:
+        PRIVATE_KEY = f.read()
+
+    now = int(time.time())
+
+    payload = {
+        "iss": "de38698d-e5cc-400b-bda1-11bec52f55ee",
+        "iat": now,
+        "exp": now + 1200,  # 20 minutes
+        "aud": "appstoreconnect-v1",
+        "scope": [
+            "GET /v1/apps",
+            "GET /v1/apps/6673915598/analyticsReportRequests"
+        ]
+    }
+
+    token = jwt.encode(
+        payload,
+        PRIVATE_KEY,
+        algorithm="ES256",
+        headers={
+            "kid": KEY_ID,
+            "typ": "JWT"
+        }
+    )
+
+    return token.decode('utf-8')
---
+++ main.py
@@ -0,0 +1,36 @@
+from jwttoken import get_token
+
+import requests
+import json
+
+token = get_token()
+print(token)
+
+url = "https://api.appstoreconnect.apple.com/v1/apps/6673915598/analyticsReportRequests"
+# url = "https://api.appstoreconnect.apple.com/v1/analyticsReportRequests"
+
+payload = json.dumps({
+  "data": {
+    "type": "analyticsReportRequests",
+    "attributes": {
+      "accessType": "ONGOING"
+    },
+    "relationships": {
+      "app": {
+        "data": {
+          "type": "apps",
+          "id": "6673915598"
+        }
+      }
+    }
+  }
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Bearer ' + token
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
+
